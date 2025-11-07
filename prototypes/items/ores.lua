ed915cd13113a868dab423dc2c61d39e6512614c
GirixK <GirixK@users.noreply.github.com>
2025-11-07 02:54:43 +0100

    make it launch

---
+++ prototypes/items/ores.lua
@@ -0,0 +1,16 @@
+data:extend({
+    {
+        type = "item",
+        name = "bauxite-ore",
+        icons = {
+            {
+                icon = "__base__/graphics/icons/iron-ore.png",
+                tint = {r=0.8, g=0.5, b=0.1, a=1},
+                icon_size = 64
+            }
+        },
+        subgroup = "raw-resource",
+        order = "a[bauxite-ore]",
+        stack_size = 50
+    }
+})
